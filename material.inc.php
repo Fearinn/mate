914a8718bcc5a944c7d03cef0278d0c29f54ab22
Matheus Gomes <105985983+Fearinn@users.noreply.github.com>
2024-01-04 15:56:38 -0300

    chore: define static info in material.inc

--- material.inc.php
+++ material.inc.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  *------
  * BGA framework: Gregory Isabelli & Emmanuel Colin & BoardGameArena
@@ -32,6 +33,56 @@ $this->card_types = array(
 
 */
 
+$this->suits = array(
+  1 => array(
+    'name' => clienttranslate('spade'),
+    'nametr' => self::_('spade')
+  ),
+  2 => array(
+    'name' => clienttranslate('heart'),
+    'nametr' => self::_('heart')
+  ),
+  3 => array(
+    'name' => clienttranslate('club'),
+    'nametr' => self::_('club')
+  ),
+  4 => array(
+    'name' => clienttranslate('diamond'),
+    'nametr' => self::_('diamond')
+  )
+);
 
+$this->suits_strength = array(
+  // clubs > spades > hearts > diamonds
+  1 => 2,
+  2 => 3,
+  3 => 1,
+  4 => 4
+);
 
+$this->values_label = array(
+  7 => '7',
+  10 => '10',
+  12 => clienttranslate('Q'),
+  13 => clienttranslate('K'),
+  14 => clienttranslate('A')
+);
 
+$this->values_strength = array(
+  // A > 10 > K > Q > 7
+  7 => 1,
+  12 => 2,
+  10 => 4,
+  13 => 3,
+  14 => 5,
+);
+
+$this->values_weight = array(
+  // The weight is used as a multiplier for scoring
+  // A = 11, K = 4, Q = 3, 7 = 7, 10 = 10
+  7 => 7,
+  10 => 10,
+  12 => 3,
+  13 => 4,
+  14 => 11,
+);
